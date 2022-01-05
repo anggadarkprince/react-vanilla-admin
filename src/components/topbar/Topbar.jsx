f5063ef6897cee35ee0125973404728cd766ed44
Angga Ari Wijaya <anggadarkprince@gmail.com>
2022-01-05 21:43:43 +0700

    add topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Topbar.css"
+import {
+    NotificationsOutlined,
+    TranslateOutlined,
+    SettingsOutlined
+} from '@mui/icons-material';
+
+export default function Topbar() {
+    return (
+        <div className='topbar'>
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className='topLogo'>Vanilla Admin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsOutlined />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <TranslateOutlined />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <SettingsOutlined />
+                    </div>
+                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
